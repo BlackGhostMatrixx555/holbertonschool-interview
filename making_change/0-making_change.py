04dfe3952716d473a56a843018dbdfb4eea5c20a
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-07-10 23:34:31 +0200

    Project of Making Change

---
+++ making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module pour résoudre le problème du rendu de monnaie (Coin Change).
+"""
+
+
+def makeChange(coins, total):
+    """
+    Calcule le nombre minimum de pièces pour atteindre un total donné.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] > total:
+        return -1
+        
+    return dp[total]
